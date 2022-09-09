c4eec92ae6a3a678c14dd255ef55a0c641c67418
AREmondev <abdurrahmanemondev@gmail.com>
2022-09-09 16:13:48 +0600

    use google fonts

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic
+} from "@expo-google-fonts/roboto";
+import AppLoading from 'expo-app-loading';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_400Regular_Italic
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }else {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontFamily: "Roboto_400Regular_Italic"}}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
